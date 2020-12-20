ecba6c8dc6cde3d6f568e9d9cc61aa9b536184fd
KatsiarynaKarneliuk <v281107@gmail.com>
2020-12-20 16:37:03 +0300

    переаисала test_main_page с помощью Page Object

---
+++ pages/main_page.py
@@ -0,0 +1,8 @@
+from pages.base_page import BasePage
+from selenium.webdriver.common.by import By
+
+"""экземпляр родительского класса"""
+class MainPage(BasePage):
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(By.CSS_SELECTOR,"#login_link")
+        login_link.click()
