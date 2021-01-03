a56954719fc17e697e920cccced007183d4fc88e
KatsiarynaKarneliuk <v281107@gmail.com>
2021-01-03 11:53:11 +0300

    basket_page was remowed

--- pages/basket_page.py
+++
@@ -1,8 +0,0 @@
-
-    def should_be_the_same_name_and_sum(self):
-        получить Name и текст товара
-        сравнить текст товара с текстом из
-        #assert self.is_element_present(*BasketPageLocators.PRODUCT_NAME), "Name is not presented"
-        #assert self.is_element_present(*BasketPageLocators.PRODUCT_SUM), "Sum link is not presented"
-
-
